a300b891344e82be576fe0db8adcdbd4037d93ba
Aaron Mahler <alm100@duke.edu>
2017-07-26 17:00:30 -0400

    Updated tests

--- tests/test_helper1.py
+++ tests/test_helper1.py
@@ -5,18 +5,18 @@ import qm10
 import pytest
 import psi4
 import numpy as np
-
 psi4.core.be_quiet()
 
 
 ### geom
-mol = psi4.geometry("""
+g1 = """
     O
     H 1 1.1
     H 1 1.1 2 104
-""")
+"""
 
 # Build a molecule
+mol = psi4.geometry(g1)
 mol.update_geometry()
 
 b1 = psi4.core.BasisSet.build(mol, target="aug-cc-pVDZ")
@@ -24,23 +24,31 @@ mints = psi4.core.MintsHelper(b1)
 
 expected = 1
 A = mints.ao_overlap()
+id2 = np.array([[1,0],[0,1]])
 
-td1 = [(mol,b1,expected), ]
+td1 = [(b1, g1, 2, expected), ]
+td2 = [(A, A, 2, id2)]
 
-@pytest.mark.parametrize("a, b, exp", td1)
-def test_hartree_fock(a, b, exp):
-    qm10.helper1.hartree_fock(b, a)
+@pytest.mark.parametrize("a, b, n, exp", td1)
+def test_hartree_fock(a, b, n, exp):
+    qm10.helper1.hartree_fock(a, b, n)
     assert(True)
 
 
 
-@pytest.mark.parametrize("a, b, exp", td1)
-def test_integrals(b, a, exp):
-    qm10.helper1.integrals(b, a)
+@pytest.mark.parametrize("a, b, n, exp", td1)
+def test_integrals(a, b, n, exp):
+    qm10.helper1.integrals(a, b)
     assert(True)
 
 
 def test_a_funct():
     qm10.helper1.a_funct(A)
     assert(True)
+
+@pytest.mark.parametrize("h1, a1, nelec, exp", td2)
+def test_core_diag(h1, a1, nelec, exp):
+    qm10.helper1.core_diag(h1, a1, nelec)
+    assert(True)
+
 
