7a8943fa893cefbbf9087a45e5c3e380210d167f
Boris <bdgc1987@gmail.com>
2017-07-26 12:27:09 -0700

    addig helper2 comments

---
+++ helper2.py
@@ -0,0 +1,36 @@
+import numpy as np
+import psi4
+
+
+def fock_diag(A, F):
+    '''
+    This functions diagonalizes the fock matrix
+    '''
+    pass
+
+def core_Hamil_diag(H, A, nel):
+    '''
+    This fuctions diagonalizes the core Hamiltonian
+    '''
+    pass
+
+def damping_func(iteration, damp_value, F_old, F_new):
+    '''
+    This fuction updates the fock matrix by a damping fuction
+    '''
+    pass
+
+def gradient_calculation(F, D, S):
+   '''
+   This functions returns the gradient and its root mean square 
+   '''
+   pass
+
+def energy_conv(F, H, D, E_old):
+   '''
+   This function builds the electronic energy from the Fock, density 
+   and core hamiltonian matrices
+   '''
+   pass
+
+
