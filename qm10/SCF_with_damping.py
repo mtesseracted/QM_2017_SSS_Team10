067e179699ae87612eb98b61fef250285c23acba
marissa <mle99958@uga.edu>
2017-07-30 17:49:56 -0400

    Implement buildInt function

--- qm10/SCF_with_damping.py
+++ qm10/SCF_with_damping.py
@@ -1,18 +1,16 @@
 import numpy as np
-import psi4
 from helper1 import *
 
 np.set_printoptions(suppress=True, precision=4)
 
-mol = psi4.geometry("""
+
+nbf, V, T, S, g, A, nuc, psi4_energy = buildInts("aug-cc-pvDZ",
+"""
 O
 H 1 1.1
 H 1 1.1 2 104
 """)
 
-# Build a molecule
-mol.update_geometry()
-mol.print_out()
 
 e_conv = 1.e-6
 d_conv = 1.e-6
@@ -20,32 +18,11 @@ nel = 5
 damp_value = 0.20
 damp_start = 5
 
-# Build a basis
-bas = psi4.core.BasisSet.build(mol, target="aug-cc-pVDZ")
-bas.print_out()
-
-# Build a MintsHelper
-mints = psi4.core.MintsHelper(bas)
-nbf = mints.nbf()
-
-if (nbf > 100):
-    raise Exception("More than 100 basis functions!")
-
-V = np.array(mints.ao_potential())
-T = np.array(mints.ao_kinetic())
-
 # Core Hamiltonian
 H = T + V
 
-S = np.array(mints.ao_overlap())
-g = np.array(mints.ao_eri())
-
 # print(S.shape)
 # print(I.shape)
-
-A = mints.ao_overlap()
-A.power(-0.5, 1.e-14)
-A = np.array(A)
 
 # print(A @ S @ A)
 
@@ -68,7 +45,7 @@ for iteration in range(25):
     grad_rms = buildgrad(F, D, S)
 
     # Build the energy
-    E_total, E_diff = buildE(F, H, D, E_old, mol.nuclear_repulsion_energy())
+    E_total, E_diff = buildE(F, H, D, E_old, nuc)
     E_old = E_total
     print("Iter=%3d  E = % 16.12f  E_diff = % 8.4e  D_diff = % 8.4e" %
             (iteration, E_total, E_diff, grad_rms))
@@ -81,7 +58,4 @@ for iteration in range(25):
 
 print("SCF has finished!\n")
 
-psi4.set_output_file("output.dat")
-psi4.set_options({"scf_type": "pk"})
-psi4_energy = psi4.energy("SCF/aug-cc-pVDZ", molecule=mol)
 print("Energy matches Psi4 %s" % np.allclose(psi4_energy, E_total))
--- qm10/helper1.py
+++ qm10/helper1.py
@@ -4,6 +4,42 @@ Hartree-Fock functions
 import psi4
 import numpy as np
 
+
+def buildInts(basis, geom):
+    mol = psi4.geometry(geom)
+    # Build a molecule
+    mol.update_geometry()
+    mol.print_out()
+
+    # Build a basis
+    bas = psi4.core.BasisSet.build(mol, target=basis)
+    bas.print_out()
+
+    # Build a MintsHelper
+    mints = psi4.core.MintsHelper(bas)
+    nbf = mints.nbf()
+    
+    if (nbf > 100):
+        raise Exception("More than 100 basis functions!")
+    
+    V = np.array(mints.ao_potential())
+    T = np.array(mints.ao_kinetic())
+    
+    A = mints.ao_overlap()
+    A.power(-0.5, 1.e-14)
+    A = np.array(A)
+
+    S = np.array(mints.ao_overlap())
+    g = np.array(mints.ao_eri())
+    
+    nuc_e = mol.nuclear_repulsion_energy()
+
+    psi4.set_output_file("output.dat")
+    psi4.set_options({"scf_type": "pk"})
+    psi4_e = psi4.energy("SCF/" + basis, molecule=mol)
+
+    return nbf, V, T, S, g, A, nuc_e, psi4_e
+
 # Diagonalize Core H
 def diag(F, A):
     Fp = A.T @ F @ A
