7bfba6aa2df18a68b6f7b8d12e6e27c6c999b5d4
Aaron Mahler <alm100@duke.edu>
2017-07-30 17:26:58 -0400

    Moved functions to module file

--- qm10/SCF_with_damping.py
+++ qm10/SCF_with_damping.py
@@ -1,5 +1,6 @@
 import numpy as np
 import psi4
+from helper1 import *
 
 np.set_printoptions(suppress=True, precision=4)
 
@@ -47,51 +48,6 @@ A.power(-0.5, 1.e-14)
 A = np.array(A)
 
 # print(A @ S @ A)
-
-
-# Diagonalize Core H
-def diag(F, A):
-    Fp = A.T @ F @ A
-    eps, Cp = np.linalg.eigh(Fp)
-    C = A @ Cp
-    return eps, C
-
-
-def updateD(H, A, nel):
-    eps, C = diag(H, A)
-    Cocc = C[:, :nel]
-    D = Cocc @ Cocc.T
-    return D
-
-def buildF(g, D, H, dampVal, F_old):
-    J = np.einsum("pqrs,rs->pq", g, D)
-    K = np.einsum("prqs,rs->pq", g, D)
-
-    F_new = H + 2.0 * J - K
-
-    # conditional iteration > start_damp
-    F = dampVal * F_old + (1.0 - dampVal) * F_new
-
-    F_old = F_new
-    # F = (damp_value) Fold + (??) Fnew
-    return F_old, F
-
-def buildgrad(F, D, S):
-    grad = F @ D @ S - S @ D @ F
-
-    grad_rms = np.mean(grad ** 2) ** 0.5
-
-    return grad_rms
-
-def buildE(F, H, D, old, nuc):
-    # here, nuc is mol.nuclear_repulsion_energy()
-    elec = np.sum((F + H) * D)
-    total = elec + nuc
-
-    diff = total - old
-
-    return total, diff
-    
 
 D = updateD(H, A, nel)
 
