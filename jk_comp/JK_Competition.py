1d069aa49e6b2bc85de32f6cdc9201b461053fc6
marissa <mle99958@uga.edu>
2017-07-31 14:29:03 -0400

    Get some JK stuff started

--- jk_comp/JK_Competition.py
+++ jk_comp/JK_Competition.py
@@ -11,10 +11,13 @@ H 1 1.1
 H 1 1.1 2 104
 """)
 
+mol.update_geometry()
+
 # Build a ERI tensor
 basis = psi4.core.BasisSet.build(mol, target="cc-pVDZ")
 mints = psi4.core.MintsHelper(basis)
 I = np.array(mints.ao_eri())
+
 
 # Symmetric random density
 nbf = I.shape[0]
@@ -33,6 +36,7 @@ K = np.random.rand(nbf, nbf)
 print("J is correct: %s" % np.allclose(J, J_ref))
 print("K is correct: %s" % np.allclose(K, K_ref))
 
+wfn = psi4.core.Wavefunction.build(mol,basis)
 
 # Get orbital basis from a Wavefunction object
 orb = wfn.basisset()
@@ -43,6 +47,7 @@ zero_bas = psi4.core.BasisSet.zero_ao_basis_set()
 # Build instance of MintsHelper
 mints = psi4.core.MintsHelper(orb)
 
+
 # Build (P|pq) raw 3-index ERIs, dimension (1, Naux, nbf, nbf)
 Qls_tilde = mints.ao_eri(zero_bas, aux, orb, orb)
 Qls_tilde = np.squeeze(Qls_tilde) # remove the 1-dimensions
@@ -50,4 +55,23 @@ Qls_tilde = np.squeeze(Qls_tilde) # remove the 1-dimensions
 metric = mints.ao_eri(zero_bas, aux, zero_bas, aux)
 metric.power(-0.5, 1.e-14)
 metric = np.squeeze(metric) # remove the 1-dimensions
+
+Qls = np.einsum('PQ,Qls->Pls', metric, Qls_tilde)
+
+
+# Coulomb matrix
+chiP = np.einsum('Pls,ls->P', Qls, D)
+Jp = np.einsum('Pmn,P->mn', Qls, chiP)
+
+print("J is correct: %s" % np.allclose(Jp, J_ref))
+
+print(Jp-J_ref)
+
+
+print(Jp.min())
+
+print(J_ref.min())
+
+
+# Exchange matrix
 
