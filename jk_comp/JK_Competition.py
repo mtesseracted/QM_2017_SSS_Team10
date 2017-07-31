b86b0d6b4c40819a3c58778107f06e028a263061
Aaron Mahler <alm100@duke.edu>
2017-07-31 14:02:19 -0400

    Add beginning of JK algo

---
+++ jk_comp/JK_Competition.py
@@ -0,0 +1,53 @@
+import numpy as np
+import psi4
+
+# Make sure we get the same random array
+np.random.seed(0)
+
+# A hydrogen molecule
+mol = psi4.geometry("""
+O
+H 1 1.1
+H 1 1.1 2 104
+""")
+
+# Build a ERI tensor
+basis = psi4.core.BasisSet.build(mol, target="cc-pVDZ")
+mints = psi4.core.MintsHelper(basis)
+I = np.array(mints.ao_eri())
+
+# Symmetric random density
+nbf = I.shape[0]
+D = np.random.rand(nbf, nbf)
+D = (D + D.T) / 2
+
+# Reference
+J_ref = np.einsum("pqrs,rs->pq", I, D)
+K_ref = np.einsum("prqs,rs->pq", I, D)
+
+# Your implementation
+J = np.random.rand(nbf, nbf)
+K = np.random.rand(nbf, nbf)
+
+# Make sure your implementation is correct
+print("J is correct: %s" % np.allclose(J, J_ref))
+print("K is correct: %s" % np.allclose(K, K_ref))
+
+
+# Get orbital basis from a Wavefunction object
+orb = wfn.basisset()
+# Build the complementary JKFIT basis for the aug-cc-pVDZ basis (for example)
+aux = psi4.core.BasisSet.build(mol, fitrole="JKFIT", other="aug-cc-pVDZ")
+# The zero basis set
+zero_bas = psi4.core.BasisSet.zero_ao_basis_set()
+# Build instance of MintsHelper
+mints = psi4.core.MintsHelper(orb)
+
+# Build (P|pq) raw 3-index ERIs, dimension (1, Naux, nbf, nbf)
+Qls_tilde = mints.ao_eri(zero_bas, aux, orb, orb)
+Qls_tilde = np.squeeze(Qls_tilde) # remove the 1-dimensions
+# Build & invert Coulomb metric, dimension (1, Naux, 1, Naux)
+metric = mints.ao_eri(zero_bas, aux, zero_bas, aux)
+metric.power(-0.5, 1.e-14)
+metric = np.squeeze(metric) # remove the 1-dimensions
+
---
+++ jk_comp/basic_mod.cpp
@@ -0,0 +1,118 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
+#include <string>
+#include <iostream>
+#include <vector>
+
+namespace py = pybind11;
+
+
+double dot_prod(const std::vector<double>& v1, const std::vector<double>& v2)
+{
+    int sz1 = v1.size(); int sz2 = v2.size();
+    if(sz1 != sz2)
+	throw std::runtime_error("Vector lengths do not match");
+    if(sz1 == 0)
+	throw std::runtime_error("Zero-length vector");
+
+    double dot = 0.0;
+    for(size_t i=0; i<sz1; i++){
+	dot += v1[i]*v2[i];
+    }
+    return dot;
+}
+
+//uncasted start method
+//double dot_prod_np(py::array v0, py::array v1)
+//cast explicitly
+double dot_prod_np(py::array_t<double> v0,
+                   py::array_t<double> v1)
+{
+    py::buffer_info v1_info = v1.request();
+    py::buffer_info v0_info = v0.request();
+
+    if(v0_info.ndim != 1)
+	throw std::runtime_error("v0 is not a vector");
+    if(v1_info.ndim != 1)
+	throw std::runtime_error("v1 is not a vector");
+    if(v0_info.shape[0] != v1_info.shape[0])
+	throw std::runtime_error("v0 and v1 are not the same len");
+
+    const double* v0_data = static_cast<double *>(v0_info.ptr);
+    const double* v1_data = static_cast<double *>(v1_info.ptr);
+
+    double sum = 0.0;
+    for(size_t i=0; i<v0_info.shape[0]; i++){
+	sum += v0_data[i]*v1_data[i];
+    }
+    return sum;
+}
+
+py::array_t<double> dgemm_numpy(double alpha,
+	           py::array_t<double> A,
+		   py::array_t<double> B)
+{
+    py::buffer_info A_info = A.request();
+    py::buffer_info B_info = B.request();
+
+    if(A_info.ndim != 2)
+	throw std::runtime_error("A is not a vector");
+    if(B_info.ndim != 2)
+	throw std::runtime_error("B is not a vector");
+
+    if(A_info.shape[1] != B_info.shape[0])
+	throw std::runtime_error("Rows of A != Cols of B");
+
+
+    size_t C_nrows = A_info.shape[0];
+    size_t C_ncols = B_info.shape[1];
+    size_t n_k = A_info.shape[1]; //same a B_info.shape[0]
+
+    const double* A_data = static_cast<double *>(A_info.ptr);
+    const double* B_data = static_cast<double *>(B_info.ptr);
+
+    std::vector<double> C_data(C_nrows*C_ncols);
+    
+    for(size_t i=0; i< C_nrows; i++){
+	for(size_t j=0; j< C_ncols; j++){
+
+	    double val = 0.0;
+	    for(size_t k=0; k<n_k; k++){
+		val += A_data[i*n_k +k] * B_data[k*C_ncols +j];
+	    }
+	    C_data[i*C_ncols +j] = alpha*val;
+
+	}
+    }
+
+    py::buffer_info Cbuf = 
+    {
+	C_data.data(),
+	sizeof(double),
+	py::format_descriptor<double>::format(),
+	2,
+	{ C_nrows, C_ncols },
+	{ C_ncols*sizeof(double), sizeof(double) }
+    };
+
+    return py::array_t<double>(Cbuf);
+}
+
+
+//Define interfaces explicitly
+PYBIND11_PLUGIN(basic_mod)
+{
+    py::module m("basic_mod", "Aaron's basic module");
+    //Fill module here
+
+    //module define(python name, c func name, help str)
+    m.def("dot_prod", &dot_prod, "Calculates dot product");
+    m.def("dot_prod_np", &dot_prod_np, "Calculates dot product");
+    m.def("dgemm_numpy", &dgemm_numpy, "Calculates matrix product");
+
+
+    return m.ptr();
+}
+
+
